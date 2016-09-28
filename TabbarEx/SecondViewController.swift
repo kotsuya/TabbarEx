2bebfe0fa078dac55f92d398fbf9484a98fe3d25
kotsuya <kotsuya00@gmail.com>
2016-09-28 11:05:15 +0900

    first commit

---
+++ TabbarEx/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  TabbarEx
+//
+//  Created by Yoo SeungHwan on 2016/09/28.
+//  Copyright © 2016年 Yoo SeungHwan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var buttonSelectedCount = 0
+    
+    @IBOutlet var buttonLabel: UILabel!
+    
+    @IBAction func buttonAction(_ sender: AnyObject) {
+        buttonSelectedCount += 1
+        buttonLabel.text = "2buttonSelected(\(buttonSelectedCount))"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
